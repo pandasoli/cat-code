a3253fb188631cdf7b04d9bba45a1994e336def0
Panda Soli <sadpandasoli@gmail.com>
2022-07-28 14:51:38 -0300

    [separated and added warnings] program to the main function

---
+++ src/highlight.py
@@ -0,0 +1,233 @@
+import sys
+import re
+import yaml
+from os import path
+from types import NoneType
+
+import openf
+import sstr
+
+
+def whereis(text, value) -> list:
+  res = []
+
+  for x in range(0, len(text)):
+    if text[x:x + len(value)] == value:
+      res.append(x)
+
+  return res
+
+def rmdup(arr) -> list:
+  res = []
+
+  for item in arr:
+    if item not in res:
+      res.append(item)
+
+  return res
+
+
+class highlight:
+  user_path = path.expanduser('~')
+  program_dir = path.dirname(path.realpath(__file__))
+  program_relative_dir = re.sub(fr'^{user_path}', '~', program_dir)
+
+  extensions = {}
+
+  current = {
+    'file': '',
+    'extension': '',
+    'config-file': '',
+    'config': {}
+  }
+
+  def __init__(self) -> None:
+    ofile = openf(f'{self.program_dir}/extensions.yml')
+
+    if ofile.status == False:
+      print('')
+      print('\033[1;31m◈ Dependency not found')
+      print(f'  \033[0;31mNot found: \033[0m{self.program_relative_dir}/extensions.yml')
+      print('')
+
+      exit()
+
+    self.extensions = yaml.safe_load(ofile.content)
+
+    pass
+
+
+  # { status: int, warnings: list<str>, res: str }
+  def text(self, text, lang = 'txt'):
+    res = { 'status': 0, 'warnings': [], 'res': '' }
+
+    if lang == 'txt':
+      res['res'] = text
+      return res
+
+    self.current['extension'] = lang
+
+    extension = self.current['extension']
+    extensions = yaml.safe_load( openf(f'{self.program_dir}/extensions.yml').content )
+
+    self.current['config-file'] = extensions[extension] if extension in extensions else extension
+    warnings = self.loadConfig()
+
+    code = sstr(text)
+    config = self.current['config']
+
+    for group in config['groups']:
+      color = group['color']
+      regexes = []
+
+      if 'regex' in group.keys():
+        regexes.append(group['regex'])
+
+      if 'regexes' in group.keys():
+        regexes += group['regexes']
+
+      for regex in regexes:
+        matches = rmdup(re.findall(regex, code.str))
+
+        for match in matches:
+          poses = whereis(code.str, match)
+
+          for pos in poses:
+            code.remove(pos, pos + len(match))
+
+            code.add(f'\033[{color}m', pos)
+            code.add(f'\033[0m', pos + len(match))
+
+    if len(warnings) > 0: res['status'] = 2
+    res['res'] = code.join()
+    res['warnings'] = warnings
+    return res
+
+  # { status: int, warnings: list<str>, res: str }
+  def file(self, file, lang = ''):
+    self.current['file'] = file
+    res = { 'status': True, 'warnings': [], 'res': '' }
+
+    fopen = openf(file)
+
+    if fopen.status == False:
+      res['status'] = 1
+      return res
+
+    res = self.text(
+      fopen.content,
+      file.split('.')[-1] if lang == '' else lang
+    )
+
+    return res
+
+
+  # str
+  def show(self, content):
+    file = self.current['file']
+
+    lines = [
+      '',
+      f'\033[1;32m✔ {file}:',
+      '  ' + '\n  '.join(content.split('\n')),
+      ''
+    ]
+
+    return '\033[0m\n'.join(lines)
+
+  # str
+  def warn(self, content, msgs):
+    file = self.current['file']
+    msgs_begin = 0
+
+    lines = [
+      '',
+      f'\033[1;33m⚠ {file}:',
+    ]
+
+    if msgs[0] == 'Not found syntax file':
+      lines[-1] += ' \033[0;33mNot found syntax file'
+      msgs_begin = 1
+
+    for x in range(msgs_begin, len(msgs)):
+      msg = msgs[x]
+      lines.append(f'  \033[33m{msg}')
+
+    if len(msgs) > 0 and msgs_begin == 0:
+      lines.append('')
+
+    lines.append('  ' + '\n  '.join(content.split('\n')))
+    lines.append('')
+    return '\033[0m\n'.join(lines)
+
+  # str
+  def error(self, msg):
+    file = self.current['file']
+
+    lines = [
+      '',
+      f"\033[1;31m✗ {file}: \033[0;31m{msg}",
+      ''
+    ]
+
+    return '\033[0m\n'.join(lines)
+
+
+  # list<str>
+  def loadConfig(self):
+    configfile = self.current['config-file']
+    warnings = []
+    config = {}
+
+    config = openf(f'{self.program_dir}/langs/{configfile}.yml')
+    if config.status == False:
+      config = { 'colors': [], 'groups': [] }
+      self.current['config'] = config
+
+      warnings.append('Not found syntax file')
+      return warnings
+
+    config = yaml.safe_load(config.content)
+
+    if type(config) is NoneType:
+      config = {}
+
+    # ◈ Validatting
+    if not 'colors' in config.keys():
+      config['colors'] = {}
+    elif type(config['colors']) is not dict:
+      config['colors'] = {}
+      warnings.append("Colors isn't an object")
+
+    if not 'groups' in config.keys():
+      config['groups'] = {}
+      warnings.append("There's no groups object")
+    elif type(config['groups']) is not list:
+      config['groups'] = []
+      warnings.append("Groups isn't a list")
+
+    for x in range(0, len(config['groups'])):
+      group = config['groups'][x]
+
+      if not 'color' in group.keys():
+        config['groups'][x]['color'] = 37
+        warnings.append("There's a group with no color")
+      if not 'regex' in group.keys() and not 'regexes' in group.keys():
+        config['groups'][x]['regex'] = ''
+        warnings.append("There's a group without regex and regexes")
+
+    # ◈ Calling color variables
+    for x in range(0, len(config['groups'])):
+      group = config['groups'][x]
+
+      if len( re.findall(r'^[0-9;]+$', str(group['color'])) ) == 0:
+        if group['color'] in config['colors'].keys():
+          config['groups'][x]['color'] = config['colors'][group['color']]
+        else:
+          config['groups'][x]['color'] = 37
+
+    self.current['config'] = config
+    return warnings
+
+
+sys.modules[__name__] = highlight
--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 import sys
-import re
-import yaml
-from types import NoneType
-from os import path
 
 import help
 import sstr
 import openf
+import highlight
 
 
 '''
@@ -22,168 +19,12 @@ import openf
     Github: pandasoli
 '''
 
-def whereis(text, value) -> list:
-  res = []
-
-  for x in range(0, len(text)):
-    if text[x:x + len(value)] == value:
-      res.append(x)
-
-  return res
-
-def rmdup(arr) -> list:
-  res = []
-
-  for item in arr:
-    if item not in res:
-      res.append(item)
-
-  return res
-
-
-class main:
-  user_path = path.expanduser('~')
-  program_dir = path.dirname(path.realpath(__file__))
-  program_relative_dir = re.sub(fr'^{user_path}', '~', program_dir)
-
-  current = {
-    'file': '',
-    'extension': '',
-    'config-file': '',
-    'config': {}
-  }
-
-  def __call__(self, file) -> None:
-    self.current['file'] = file
-    self.current['extension'] = file.split('.')[-1]
-
-    extension = self.current['extension']
-    extensions = yaml.safe_load( openf(f'{self.program_dir}/extensions.yml').content )
-
-    self.current['config-file'] = extensions[extension] if extension in extensions else extension
-    configStatus = self.loadConfig()
-
-
-    fileOpen = openf(file)
-
-    if fileOpen.status == False:
-      print('')
-      print(f"\033[1;31m◈ {file}\033[0;31m: Doesn't found")
-      print('')
-      return
-
-    code = sstr(fileOpen.content)
-    config = self.current['config']
-
-    if configStatus != 0:
-      if configStatus == 2:
-        self.show(fileOpen.content, False)
-      return
-
-    for group in config['groups']:
-      color = group['color']
-      regexes = []
-
-      if 'regex' in group.keys():
-        regexes.append(group['regex'])
-
-      if 'regexes' in group.keys():
-        regexes += group['regexes']
-
-      for regex in regexes:
-        matches = rmdup(re.findall(regex, code.str))
-
-        for match in matches:
-          poses = whereis(code.str, match)
-
-          for pos in poses:
-            code.remove(pos, pos + len(match))
-
-            code.add(f'\033[{color}m', pos)
-            code.add(f'\033[0m', pos + len(match))
-
-    self.show(code.join())
-
-  def show(self, content, foundHL = True) -> None:
-    file = self.current['file']
-
-    print('')
-    print(f'\033[1;32m✔ {file}:\033[0m', end = '')
-
-    if foundHL == False:
-      print(f" \033[33mNo highlight found\033[0m")
-    else:
-      print('')
-
-    print('  ' + '\n  '.join(content.split('\n')))
-    print('')
-
-  def error(self, message) -> 1:
-    file = self.current['file']
-    configfile = self.current['config-file']
-
-    lines = [
-      '',
-      f'\033[1;31m✗ {file}',
-      f'  \033[31m{self.program_relative_dir}/langs/{configfile}.yml\033[37m: Got an unexpected architecture',
-      f'  {message}',
-      '',
-      '  \033[1;37m◈ To more help',
-      '    catc \033[33m--help',
-      ''
-    ]
-
-    for line in lines:
-      print(f'{line}\033[0m')
-
-    return 1
-
-  def loadConfig(self) -> int:
-    configfile = self.current['config-file']
-    config = {}
-
-    config = openf(f'{self.program_dir}/langs/{configfile}.yml')
-    if config.status == False: return 2
-
-    config = yaml.safe_load( config.content )
-
-    # ◈ Validatting
-    if not 'colors' in config.keys():
-      config['colors'] = {}
-    elif type(config['colors']) is not dict:
-      return self.error("Colors isn't an object")
-
-    if not 'groups' in config.keys():
-      return self.error("There's no groups object")
-    # elif type(config['groups']) is not list:
-      # return error("Groups isn't a list")
-
-    for group in config['groups']:
-      if not 'color' in group.keys():
-        return self.error("There's a group with no color")
-      if not 'regex' in group.keys() and not 'regexes' in group.keys():
-        return self.error("There's a group without regex and regexes")
-
-    # ◈ Calling color variables
-    for x in range(0, len(config['groups'])):
-      group = config['groups'][x]
-
-      if len( re.findall(r'^[0-9;]+$', str(group['color'])) ) == 0:
-        if group['color'] in config['colors'].keys():
-          config['groups'][x]['color'] = config['colors'][group['color']]
-        else:
-          config['groups'][x]['color'] = 37
-
-    self.current['config'] = config
-    return 0
-
-
-if __name__ == '__main__':
+def main():
   if len(sys.argv) == 1:
     lines = [
       '',
-      f'\033[1;31m✗ No params found',
-      f'  Type: catc \033[33m--help',
+      f'\033[1;31m✗ No params found:',
+      '  Type \033[31mcatc \033[33m--help\033[0m for help',
       ''
     ]
 
@@ -191,10 +32,34 @@ if __name__ == '__main__':
       print(f'{line}\033[0m')
     exit()
 
-  fun = main()
+  code = highlight()
 
   for file in sys.argv[1:]:
     if file == '--help' or file == '-h':
       help()
     else:
-      fun(file)
+      splited = file.split(':')
+      file = splited[0]
+      syntax = ''
+
+      if len(splited) > 1:
+        syntax = splited[1]
+
+      res = code.file(file, syntax)
+
+      if res['status'] == 0:
+        print(
+          code.show(res['res'])
+        )
+      elif res['status'] == 1:
+        print(
+          code.error("Doesn't found")
+        )
+      elif res['status'] == 2:
+        print(
+          code.warn(res['res'], res['warnings'])
+        )
+
+
+if __name__ == '__main__':
+  main()
