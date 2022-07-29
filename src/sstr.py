318747a26f27b8493ab2d5ab1763855fda1d268e
Panda Soli <sadpandasoli@gmail.com>
2022-07-28 23:37:26 -0300

    [updated] update

--- src/sstr.py
+++ src/sstr.py
@@ -2,60 +2,50 @@ import sys
 import re
 
 
-def insertat(arr, item, at) -> list:
-  res = []
-
-  for x in range(0, len(arr)):
-    if x == at:
-      res.append(item)
-    res.append(arr[x])
-
-  return res
-
-
-class main:
+class sstr:
   str = ''
-  colors = []
+  colors = {}
 
-  def __init__(self, str) -> None:
+  def __init__(self, str):
     self.str = str
+    self.colors = {}
 
-  def add(self, cl, pos) -> None:
-    color_regex = r'\033\[[0-9;]+m'
-    content = {
-      'value': cl,
-      'pos': pos
-    }
+  def add(self, cl, pos):
+    self.colors[pos] = cl
 
-    if len(self.colors) == 0 or self.colors[-1]['pos'] < pos:
-      self.colors.append(content)
-      return
-
-    for x in range(0, len(self.colors)):
-      color = self.colors[x]
-
-      if color['pos'] > pos:
-        self.colors = insertat(self.colors, content, x)
-        break
-
-  def remove(self, start, end) -> None:
-    res = []
-
-    for color in self.colors:
-      if color['pos'] < start or color['pos'] > end:
-        res.append(color)
-
-    self.colors = res
+  def remove(self, start, end):
+    for x in range(start, end):
+      if x in self.colors:
+        del self.colors[x]
 
   def join(self) -> str:
     res = self.str
+    colors = dict( self.colors )
+    csum = 0
 
-    sumOfColors = 0
-    for color in self.colors:
-      res = res[:color['pos'] + sumOfColors] + color['value'] + res[color['pos'] + sumOfColors:]
-      sumOfColors += len(color['value'])
+    def getmin(arr):
+      res = arr[0]
+
+      for item in arr:
+        if item < res:
+          res = item
+
+      return res
+
+    for _ in self.colors.items():
+      ps = getmin(list(colors.keys()))
+      cl = colors[ps]
+
+      res = res[:ps + csum] + cl + res[ps + csum:]
+      csum += len(cl)
+
+      del colors[ps]
+
+    # f = open('[scd_str] join res.cache.txt', 'w')
+    # f.write(res)
+    # f.close()
 
     return res
 
 
-sys.modules[__name__] = main
+sys.modules[__name__] = sstr
